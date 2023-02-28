a9fc2766bacc27eac3d6c09db13ac135db96a52c
wanglingxiao <lingxiao.wang@ucdconnect.ie>
2023-02-28 17:08:19 -0500

    login_function

--- project/src/main/java/com/example/ece651/controller/UserController.java
+++ project/src/main/java/com/example/ece651/controller/UserController.java
@@ -3,11 +3,13 @@ package com.example.ece651.controller;
 
 import com.example.ece651.domain.User;
 import com.example.ece651.service.UserServiceImpl;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +26,27 @@ public class UserController {
         user1.setPassword(user.get("password"));
         user1.setPhoneNumber(user.get("phoneNumber"));
         return userService.AddUser(user1);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody Map<String,String> user){
+        String username = user.get("username");
+        String password = user.get("password");
+        if (username == null || password == null) {
+            return "Error_NULL Exception";
+        }
+        List<User> userlist = userService.FindUserByUsername(username);
+        for(int i=0;i<userlist.size();i++){
+            User current_user = userlist.get(i);
+            String cur_username = current_user.getUsername();
+            String cur_password = current_user.getPassword();
+            //System.out.println(password);
+            //System.out.println(cur_username+" "+cur_password);
+            if (cur_password.equals(password)){
+                return "login successful";
+            }
+        }
+        return "Not found this user in the system";
     }
 
 
